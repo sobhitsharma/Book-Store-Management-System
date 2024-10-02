5a8ae3304627541110b4cfc7101175c375162f60
sobhitsharma <sobhitsharma82090@gmail.com>
2024-10-02 12:19:41 +0530

    first-commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL ='mongodb+srv://sobhitsharma82090:0YSWDR5BrOJHjfzY@cluster0.bkmnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
